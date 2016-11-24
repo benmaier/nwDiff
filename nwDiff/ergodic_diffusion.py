5e64a2b473a2f5b8efd7fc0f202529b616910185
Benjamin Maier <benjaminfrankmaier@gmail.com>
2016-11-24 17:08:51 +0100

    some test

--- nwDiff/ergodic_diffusion.py
+++ nwDiff/ergodic_diffusion.py
@@ -9,7 +9,7 @@ import networkx as nx
 
 class ErgodicDiffusion():
 
-    def __init__(self,G,max_ratio_visited=None,max_diff_time=None,N_walker=10000,initial_seeds=None,record_jump_distance=False):
+    def __init__(self,G,N_walker=10000,initial_seeds=None,record_jump_distance=False):
         
         self.N_walker = N_walker
 
@@ -22,10 +22,10 @@ class ErgodicDiffusion():
 
 
         if initial_seeds is None:
-            initial_node = random.randint(self.N_nodes)
-            self.initial_nodes = [ initial_node for walker in xrange(self.N_walkter) ]
+            initial_node = random.randint(self.N_walker)
+            self.initial_nodes = [ initial_node for walker in xrange(self.N_walker) ]
         elif initial_seeds == "random":
-            self.initial_nodes = random.randint(self.N_nodes,size=self.N_walker)
+            self.initial_nodes = random.randint(self.N_nodes,size=(self.N_walker,))
         elif hasattr(initial_seeds,"__len__"):
             self.initial_nodes = initial_seeds
 
@@ -36,16 +36,18 @@ class ErgodicDiffusion():
 
     def rewind(self):
 
-        self.visited_nodes = [ set() for walker in xrange(self.N_walkter) ]
+        self.visited_nodes = [ set() for walker in xrange(self.N_walker) ]
         for walker in xrange(self.N_walker):
-            self.visited_nodes[walker].insert(self.initial_nodes[walker])
+            self.visited_nodes[walker].add(self.initial_nodes[walker])
 
         if self.record_jump_distance:
             self.jump_distances = []
 
-        self.remaining_walkers = set(range(self.xrange()))
+        self.remaining_walkers = set(range(self.N_walker))
 
-        self.current_nodes = initial_nodes
+        self.current_nodes = self.initial_nodes
+
+
 
 
     def timestep(self):
@@ -65,7 +67,7 @@ class ErgodicDiffusion():
                 self.jump_distances.append( distance )
 
             self.current_nodes[walker] = neigh
-            self.visited_nodes[walker].insert(neigh)
+            self.visited_nodes[walker].add(neigh)
 
     #def simulation(self,tmax):
 
@@ -82,6 +84,7 @@ class ErgodicDiffusion():
         nmax = self.N_nodes
 
         for t in xrange(tmax):
+
             self.timestep()
 
             finished_walkers = []
@@ -90,12 +93,74 @@ class ErgodicDiffusion():
                 if trajectories[walker,t] == nmax:
                     finished_walkers.append(walker)
 
-            self.remaining_walkers -= finished_walkers
+            self.remaining_walkers -= set(finished_walkers)
 
         if get_trajectories:
             return trajectories
         else:
             return trajectories[:,-1]
+
+    def simulate_till_max_visited_nodes(self,nmax,snapshots_at=[]):
+
+        snapshots = []
+
+        t = 0
+
+        self.arrival_times = np.zeros((self.N_walker,),dtype=int)
+
+        while len(self.remaining_walkers)>0:
+
+            finished_walkers = []
+            for walker in self.remaining_walkers:
+                visited = len(self.visited_nodes[walker])
+                if visited == nmax:
+                    finished_walkers.append(walker)
+                    self.arrival_times[walker] = t
+
+            self.remaining_walkers -= set(finished_walkers)
+
+            if t in snapshots_at:
+                snapshots.append( np.array([len(self.visited_nodes[walker]) for walker in xrange(self.N_walker) ],dtype=int) )
+
+            t += 1
+
+            self.timestep()
+
+        if len(snapshots_at)>0:
+            return self.arrival_times, snapshots
+        else:
+            return self.arrival_times
+
+
+def step_histogram(data,**kwargs):
+    steps, bins = np.histogram(data,**kwargs)
+
+    y = np.concatenate((
+                            np.array([0.]),
+                            steps,
+                            np.array([steps[-1],0]),
+                        ))
+    x = np.concatenate((
+                            np.array([bins[0]]),
+                            bins,
+                            np.array([bins[-1]]),
+                      ))
+
+    return y,x
+
+def cut_histogram(data,bins,**kwargs):
+
+    assert np.std(bins[1:]-bins[:-1]) > 0.
+    
+    db = bins[1]-bins[0]
+    steps, bins = np.histogram(data,bins=bins,**kwargs)
+
+    indices = np.nonzero(steps)[0]
+    min_i = indices[0]
+    max_i = indices[-1]
+
+    return steps[min_i:max_i+1], min_i, max_i
+    
 
 
 
@@ -110,15 +175,51 @@ if __name__ == "__main__":
 
     sns.set_style("white")
 
+    N_walker = 50
+    tmax = 2000
+
     N = 10
     B = 8
     L = 3
     N = B**L
     k = 10
-    xi = 0.25
+    xi = 8
     p_ER = float(k)/(N-1)
     #G = nx.fast_gnp_random_graph(N,p_ER)
 
     #G = mhrn.fast_mhr_graph(B,L,k,xi)
     G = mhrn.continuous_hierarchical_graph(B**L,k,np.log(xi)/np.log(B),redistribute_probability=False)
+
+    props = nprops(G,use_giant_component=True)
+
+    diff = ErgodicDiffusion(props.G,N_walker=N_walker,initial_seeds="random")
+
+    traj = diff.simulate_till_max_time(tmax,get_trajectories=True)
+
+    fig,ax = pl.subplots(1,3,figsize=(12,5))
+
+    t_ = np.arange(tmax)
+    
+    for walker in xrange(N_walker):
+        ax[0].plot(t_,traj[walker,:],'b',alpha=0.1)
+
+    diff.rewind()
+
+    snapshot_times = [ 500, 1000, 2000, 4000 ]
+
+    arrival_times,snapshots = diff.simulate_till_max_visited_nodes(diff.N_nodes,snapshot_times)
+
+    data, bins = step_histogram(arrival_times,bins=np.arange(42)*500)
+
+    ax[1].step(bins,data,where='post')
+
+    for it,t in enumerate(snapshot_times):
+        data, bins = step_histogram(snapshots[it],bins=np.arange(56)*10)
+        #data, bins = np.histogram(snapshots[it])
+        #data = np.concatenate((np.data,np.array([data[-1]])))
+
+        ax[2].step(bins,data,where='post')
+
+    pl.show()
+
 
